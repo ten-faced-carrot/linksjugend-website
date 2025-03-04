11a4485d469dbc2c1c73290016bc398e7a8dc824
June <stupidjane@tutanota.com>
2025-03-04 21:18:49 +0100

    feat: Removed useless things

--- app.py
+++ app.py
@@ -240,110 +240,22 @@ def new_torrent():
     return render_template("newtorrent.html")
 
 
-from copy import deepcopy
-from threading import Thread
-from flask.wrappers import Request
-import libtorrent as lt
-from pickle import dumps
 kt = {}
 
 ROOT = "localhost"
 RP = 5000
-
-@socketio.on("am_i_done")
-def aid(id):
-    if os.path.exists(path := os.path.abspath(f"lt/{id}")) and len(os.listdir(path)) > 0:
-        if os.path.exists(os.path.join(path, "magnet")):
-            emit("gt", open(os.path.join(path, "magnet")).read())
-        else:
-            emit("gt", f'/ljcdn/torrent/{id}.torrent')
-
-@app.route("/ljcdn/torrent/<id>.torrent")
-def getTorrent(id):
-    path = os.path.abspath(f"lt/{id}")
-    print(path, os.listdir(path))
-    if os.path.exists(path) and len(os.listdir(path)) > 0:
-        return send_from_directory(f'lt/{id}', os.listdir(path)[0])
-    abort(404)
-ses = lt.session()
-ses.listen_on(6881, 6891)
-
-
-
-def add_torrent(source, id):
-    SAVE_PATH  = "/tmp"
-    if source.startswith("magnet:?"):
-        params = lt.parse_magnet_uri(source)
-        params.save_path = SAVE_PATH
-        
-        print(params)
-        handle = ses.add_torrent(params)
-    else:
-        info = lt.torrent_info(source)
-        handle = ses.add_torrent({"ti": info, "save_path": "."})
-
-    info = handle.get_torrent_info()
-    while not handle.has_metadata():
-        ...
-    torrent = Torrent()
-    torrent.info_hash = str(info.info_hash())
-    torrent.seeders = 0
-    torrent.peers = 0
-    torrent.t_id = id
-    db.session.add(torrent)
-    db.session.commit()
-
-def upload_and_torrent(environ, id, current_user: User):
-    #with app.request_context(environ):
-        socketio.emit("btrack_bgn", (f'Beginning to upload files', id))
-        fs = lt.file_storage()
-        os.makedirs(os.path.join("torrent_cache", id), exist_ok=True)
-        os.makedirs(os.path.join('lt', id))
-        for file in request.files.getlist('files'):
-            print(file.filename)
-            file.save(os.path.join("torrent_cache", id) + "/"+secure_filename(file.filename))
-            lt.add_files(fs, os.path.join("torrent_cache", id)+ "/"+secure_filename(file.filename))
-            socketio.emit("btrack_bgn", (f'Cached: {file.filename}', id))
-        t = lt.create_torrent(fs)
-        t.set_creator(request.form.get("creator", current_user.name))
-        t.set_comment(request.form.get("comment", ""))
-        t.add_tracker(f"http://{ROOT}:6969/announce")
-        t.set_priv(False)
-        lt.set_piece_hashes(t, os.path.dirname(f'torrent_cache/{id}/.'))
-        torrent_data = t.generate()
-        with open(f'lt/{id}/{secure_filename(request.form["name"]).replace(" ",".")}.torrent', 'wb+') as f:
-            f.write(lt.bencode(torrent_data))
-        if request.form.get("tfile_magnet") == "magnet":
-            manget_link = lt.make_magnet_uri(lt.torrent_info(f'lt/{id}/{secure_filename(request.form["name"]).replace(" ",".")}.torrent'))
-            os.rename(f'lt/{id}/{secure_filename(request.form["name"]).replace(" ",".")}.torrent', f'lt/{id}/magnet')
-            with open(f'lt/{id}/magnet', "w+") as f:
-                f.write(manget_link)
-            add_torrent(manget_link, id)
-        else:
-            add_torrent(f'lt/{id}/{secure_filename(request.form["name"]).replace(" ",".")}.torrent', id)
-
-        socketio.emit("btrack_done", (f'', id))
-import requests
-
-@app.route("/lj-backend/lj-torrentd/tracker")
-@login_required
-def track():
-    r = requests.get(f"http://{ROOT}:6969/stats").json()
-    return render_template("tracklist.html", torrents = r)
-
-@app.route("/lj-backend/lj-torrentd/upload", methods=["POST"])
-@login_required
-def upload_torrent():
-    id = ''.join([secrets.choice(string.ascii_letters) for _ in range(30)])
-    upload_and_torrent(request.environ.copy(), id, current_user)
-    return render_template("torrent_wait.html", id=id)
 
 
 @app.route("/mitmachen")
 def mm():
     if current_user.is_authenticated:
         return redirect("/lj-backend")
-    abort(404) 
+    return """
+<meta http-equiv="refresh" content"10;/login">
+Hier könnten sich Neumitglieder*innen registrieren - Aber das ist noch nicht implementiert. Du wirst stattdessen weitergeleitet zur anmeldeseite<br>
+
+Solltest du nicht nach wenigen sekunden weitergeleitet werden kommst du <a href="/login">hier</a> zur anmeldung
+"""
 
 
 if __name__ == "__main__":
